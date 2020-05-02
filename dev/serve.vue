423a31a77362c4ad4ee3bd7036861de1f034dd26
Pradeep <webreinvent@gmail.com>
2020-05-02 17:48:07 +0530

    package generated

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import { VaahvueSample } from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VaahvueSample,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vaahvue-sample />
+  </div>
+</template>
