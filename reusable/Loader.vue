b1dec0869b23dc18b6885938afa0b5c41980dfd1
Pradeep <webreinvent@gmail.com>
2021-01-15 18:52:39 +0530

    Fixed: loader

--- reusable/Loader.vue
+++ reusable/Loader.vue
@@ -1,20 +1,9 @@
 <template>
-    <div>
-        <div style="max-width:300px; max-height: 60px;">
-            <content-loader
-                    :height="50"
-                    :width="300"
-                    :speed="1"
-                    primaryColor="#f3f3f3"
-                    secondaryColor="#ecebeb"
-            >
-                <rect x="25" y="10" rx="5" ry="5" width="30%" height="5" />
-                <rect x="25" y="20" rx="5" ry="5" width="50%" height="5" />
-                <rect x="25" y="30" rx="5" ry="5" width="80%" height="5" />
-
-            </content-loader>
-        </div>
-    </div>
+    <content-loader>
+        <rect x="25" y="10" rx="5" ry="5" width="30%" height="5" />
+        <rect x="25" y="20" rx="5" ry="5" width="50%" height="5" />
+        <rect x="25" y="30" rx="5" ry="5" width="80%" height="5" />
+    </content-loader>
 </template>
 
 <script>
