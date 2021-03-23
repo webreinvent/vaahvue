3c5ff911aa1accd62cfa8c74c43e6982d271d317
Ameey <ameey.k@webreinvent.com>
2021-03-23 22:44:21 +0530

    CKEditor

--- reusable/content-fields/VvCKEditor.vue
+++ reusable/content-fields/VvCKEditor.vue
@@ -24,6 +24,11 @@
 <script>
 
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
+
+window.ClassicEditor = ClassicEditor;
+window.SimpleUploadAdapter = SimpleUploadAdapter;
+
 
 export default {
     props:{
@@ -66,7 +71,22 @@ export default {
         let obj = {
             editor: ClassicEditor,
             editorData: "",
+
             editorConfig: {
+                plugins: [ SimpleUploadAdapter ],
+                simpleUpload: {
+                    // The URL that the images are uploaded to.
+                    uploadUrl: 'http://example.com',
+
+                    // Enable the XMLHttpRequest.withCredentials property.
+                    withCredentials: true,
+
+                    // Headers sent along with the XMLHttpRequest to the upload server.
+                    headers: {
+                        'X-CSRF-TOKEN': 'CSRF-Token',
+                        Authorization: 'Bearer <JSON Web Token>'
+                    }
+                }
             }
         };
 
