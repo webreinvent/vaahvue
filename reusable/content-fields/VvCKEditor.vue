1a3c8c882576f75241af2254ce409add79342672
Ameey <ameey.k@webreinvent.com>
2021-03-26 13:49:31 +0530

    Upload code is added

--- reusable/content-fields/VvCKEditor.vue
+++ reusable/content-fields/VvCKEditor.vue
@@ -24,11 +24,104 @@
 <script>
 
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
+//import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 
 window.ClassicEditor = ClassicEditor;
-window.SimpleUploadAdapter = SimpleUploadAdapter;
+//window.SimpleUploadAdapter = SimpleUploadAdapter;
 
+
+
+class MyUploadAdapter {
+    constructor(props) {
+        // CKEditor 5's FileLoader instance.
+        this.loader = props;
+        // URL where to send files.
+        this.url = `http://localhost/jalapeno-website-dev/public/backend/cms/media/upload`;
+    }
+
+    // Starts the upload process.
+    upload() {
+        return new Promise((resolve, reject) => {
+            this._initRequest();
+            this._initListeners(resolve, reject);
+            this._sendRequest();
+        } );
+    }
+
+    // Aborts the upload process.
+    abort() {
+        if ( this.xhr ) {
+            this.xhr.abort();
+        }
+    }
+
+    // Example implementation using XMLHttpRequest.
+    _initRequest() {
+        const xhr = this.xhr = new XMLHttpRequest();
+
+        let token = $('#_token').attr('content');
+
+        console.log('--->token', token);
+
+        xhr.open('POST', this.url, true);
+        xhr.responseType = 'json';
+        xhr.setRequestHeader('Access-Control-Allow-Origin', '*')
+        xhr.setRequestHeader('Authorization', $('#_token').attr('content'))
+        xhr.setRequestHeader('X-CSRF-TOKEN', $('#_token').attr('content'))
+    }
+
+    // Initializes XMLHttpRequest listeners.
+    _initListeners( resolve, reject ) {
+        const xhr = this.xhr;
+        const loader = this.loader;
+        const genericErrorText = 'Couldn\'t upload file:' + ` ${ loader.file.name }.`;
+
+        xhr.addEventListener( 'error', () => reject( genericErrorText ) );
+        xhr.addEventListener( 'abort', () => reject() );
+        xhr.addEventListener( 'load', () => {
+            const response = xhr.response;
+            if ( !response || response.error ) {
+                return reject( response && response.error ? response.error.message : genericErrorText );
+            }
+
+            console.log('--->response', response);
+
+            // If the upload is successful, resolve the upload promise with an object containing
+            // at least the "default" URL, pointing to the image on the server.
+            resolve({
+                default: response.data.full_url
+            });
+        } );
+
+        if ( xhr.upload ) {
+            xhr.upload.addEventListener( 'progress', evt => {
+                if ( evt.lengthComputable ) {
+                    loader.uploadTotal = evt.total;
+                    loader.uploaded = evt.loaded;
+                }
+            } );
+        }
+    }
+
+    // Prepares the data and sends the request.
+    _sendRequest() {
+        const data = new FormData();
+
+        this.loader.file.then(result => {
+                data.append('file', result);
+                this.xhr.send(data);
+            }
+        )
+    }
+
+};
+
+
+function MyCustomUploadAdapterPlugin(editor) {
+    editor.plugins.get( 'FileRepository' ).createUploadAdapter = (loader) => {
+        return new MyUploadAdapter(loader)
+    }
+}
 
 export default {
     props:{
@@ -73,7 +166,8 @@ export default {
             editorData: "",
 
             editorConfig: {
-                plugins: [ SimpleUploadAdapter ],
+                //plugins: [ SimpleUploadAdapter ],
+                extraPlugins: [ MyCustomUploadAdapterPlugin ],
                 simpleUpload: {
                     // The URL that the images are uploaded to.
                     uploadUrl: 'http://example.com',
