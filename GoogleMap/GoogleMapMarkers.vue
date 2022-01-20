1df18991d6c579c984f9098d1cc64102bf898435
Pradeep Kumar <webreinvent@gmail.com>
2022-01-20 19:32:01 +0530

    Added: GoogleMapMarker component

---
+++ GoogleMap/GoogleMapMarkers.vue
@@ -0,0 +1,350 @@
+<template>
+    <div>
+        <div class="switches-section is-clearfix" v-if="show_controls">
+            <b-field class="is-pulled-right" grouped>
+                <b-switch v-model="set_map_center"
+                          type="is-success">
+                    Set Map Center
+                </b-switch>
+
+                <!--<b-switch v-model="hide_labels"
+                          :disabled="set_map_center"
+                          type="is-success">
+                    Hide Labels
+                </b-switch>-->
+
+                <b-switch v-model="is_add_marker"
+                          :disabled="set_map_center || is_remove_marker"
+                          type="is-success">
+                    <span v-if="!this.is_edit_ready">Add Markers</span>
+                    <span v-else>Add/Edit Markers</span>
+                </b-switch>
+
+                <b-switch v-model="is_remove_marker"
+                          :disabled="set_map_center"
+                          type="is-success">
+                    Remove Markers
+                </b-switch>
+            </b-field>
+        </div>
+
+        <div class="has-margin-10">
+            <b-message size="is-small" type="is-info" v-if="set_map_center">
+                Drag map and click to set map center
+            </b-message>
+            <b-message  size="is-small" type="is-info" v-if="is_marker_mode">
+                <span v-if="!is_remove_marker">Now, you can click on map to add markers</span>
+                <span v-else>Click on any marker to remove it</span>
+            </b-message>
+        </div>
+
+
+        <GmapMap
+            ref="map"
+            :key="map_key"
+            :center='center'
+            :zoom='zoom'
+            :style='custom_style'
+            :map-type-id='map_type_id'
+            :option="{
+                rotateControl: true,
+                streetViewControl: false,
+            }"
+            @click="mapClicked"
+        >
+            <!--@NOTE
+            To add marker labels add the following attributes
+            :label="!hide_labels?{text: m.name, color: '#fff', fontWeight: '900', y:'100px'}:null"
+            -->
+
+            <!--Flag Markers-->
+            <GmapMarker
+                v-if="!set_map_center"
+                :key="index"
+                v-for="(m, index) in markers"
+                :position="m.position"
+                :clickable="is_marker_mode"
+                :draggable="is_marker_mode"
+                :icon="image_url+'/'+marker_icon"
+                @click="markerClicked(m,index)"
+                @dragend="markerDragged($event, m)"
+            />
+            <!--/Flag Markers-->
+
+
+            <!--Horse Position-->
+            <!--<GmapMarker
+                :key="index"
+                :position="horse_position"
+                :icon="image_url+'/'+marker_icon"
+            />-->
+            <!--Horse Position-->
+
+
+
+
+        </GmapMap>
+    </div>
+</template>
+
+<script>
+    import {gmapApi} from 'vue2-google-maps'
+
+    export default {
+        name: 'GoogleMapMarkers',
+        props: {
+            map_key: Number,
+            latitude: {
+                type: String|Number,
+                default: 0
+            },
+            longitude: {
+                type: String|Number,
+                default: 0
+            },
+            zoom: {
+                type: String|Number,
+                default: 16
+            },
+            custom_style: {
+                type: String,
+                default: 'width:100%;  height: 400px;'
+            },
+
+            map_type_id: {
+                type: String,
+                default: 'satellite'
+            },
+            show_controls:{
+                type: Boolean,
+                default: true,
+            },
+            is_rotatable:{
+                type: Boolean,
+                default: true,
+            },
+            image_url:{
+                type: String,
+                default: null
+            },
+            marker_icon:{
+                type: String,
+                default: null
+            },
+            rotate_left_icon:{
+                type: String,
+                default: null
+            },
+            rotate_right_icon:{
+                type: String,
+                default: null
+            },
+            heading:{
+                type: String|Number,
+                default: '0'
+            },
+            tilt:{
+                type: String|Number,
+                default: '0'
+            },
+            is_edit_ready:{ // sets add marker default to true and changes its switch label
+                type: Boolean,
+                default: false
+            }
+        },
+        computed: {
+            google : gmapApi,
+            is_marker_mode(){
+                return this.is_add_marker || this.is_remove_marker;
+            },
+            center(){
+                return {
+                    lat: parseFloat(this.latitude),
+                    lng: parseFloat(this.longitude)
+                }
+            }
+        },
+        data() {
+            return {
+                currentPlace: null,
+                places: [],
+                set_map_center: false,
+                hide_labels: true,
+                is_add_marker: this.is_edit_ready,
+                is_remove_marker: false,
+            }
+        },
+        inject: ['markers'],
+        watch:{
+            set_map_center(new_val){
+                if(new_val){
+                    this.hide_labels = false;
+                    this.is_add_marker = false;
+                    this.is_remove_marker = false;
+                }
+            },
+        },
+
+        mounted() {
+            this.onLoad();
+        },
+        methods: {
+            //-----------------------------------------------------------------------------
+            onLoad(){
+                this.setRotateControls();
+            },
+            //-----------------------------------------------------------------------------
+
+
+            //-----------------------------------------------------------------------------
+            setRotateControls(place) {
+                let self = this;
+
+                this.$refs.map.$mapPromise.then((map) => {
+                    const buttons = [
+                        ["Rotate Left", "rotate", 10, self.google.maps.ControlPosition.LEFT_CENTER],
+                        ["Rotate Right", "rotate", -10, self.google.maps.ControlPosition.RIGHT_CENTER],
+                    ];
+
+                    buttons.forEach(([text, mode, amount, position]) => {
+                        const controlDiv = document.createElement("div");
+                        const controlUI = document.createElement("b-button");
+
+                        controlUI.classList.add("ui-button");
+                        if (`${text}` === 'Rotate Left') {
+                            controlUI.innerHTML = `<img src='`+self.image_url+`/`+self.rotate_left_icon+`' width='40px'/>`;
+                        } else {
+                            controlUI.innerHTML = `<img src='`+self.image_url+`/`+self.rotate_right_icon+`' width='40px'/>`;
+                        }
+
+
+                        controlUI.addEventListener("click", () => {
+                            self.rotateMap(amount);
+                        });
+                        controlDiv.appendChild(controlUI);
+                        map.controls[position].push(controlDiv);
+                    });
+                });
+
+            },
+            //-----------------------------------------------------------------------------
+
+
+            //-----------------------------------------------------------------------------
+            mapClicked(e){
+                if(!this.set_map_center){
+                    this.addMarker(e)
+                }
+                else{
+
+                    let pos = {
+                        lat: e.latLng.lat(),
+                        lng: e.latLng.lng(),
+                    };
+                    let self = this;
+
+                    this.$refs.map.$mapPromise.then((map) => {
+                        map.panTo(pos);
+                        map.setZoom(self.zoom);
+                        self.$emit('centerChanged',pos);
+                    });
+
+                }
+            },
+            //-----------------------------------------------------------------------------
+
+
+            //-----------------------------------------------------------------------------
+            addMarker(e){
+                if(!this.is_add_marker) return;
+
+                this.$buefy.dialog.prompt({
+                    message: `Please enter marker name`,
+                    inputAttrs: {
+                        placeholder: 'e.g. Marker 1',
+                        maxlength: 10
+                    },
+                    trapFocus: true,
+                    onConfirm: (name) => {
+                        const marker = {
+                            lat: e.latLng.lat(),
+                            lng: e.latLng.lng(),
+                        };
+                        this.markers.push({
+                            name: name,
+                            position: marker
+                        });
+
+                        //this.$emit('markerAdded',this.markers);
+                    }
+                });
+            },
+            //-----------------------------------------------------------------------------
+            markerClicked(marker, index){
+                if(!this.is_marker_mode) return;
+
+                if(this.is_remove_marker){
+                    this.removeMarker(index);
+                    return;
+                }
+
+                this.$buefy.dialog.prompt({
+                    message: `Rename marker: `,
+                    inputAttrs: {
+                        placeholder: 'e.g. Marker 1',
+                        maxlength: 10,
+                        value: marker.name
+                    },
+                    trapFocus: true,
+                    onConfirm: (name) => {
+                        marker.name = name;
+                    }
+                });
+            },
+            //-----------------------------------------------------------------------------
+
+            //-----------------------------------------------------------------------------
+            markerDragged(e, marker){
+                //if(!this.is_marker_mode) return;
+
+                marker.position = e.latLng;
+            },
+            //-----------------------------------------------------------------------------
+
+
+            //-----------------------------------------------------------------------------
+            removeMarker(index){
+                this.markers.splice(index, 1);
+            },
+            //-----------------------------------------------------------------------------
+
+
+            //-----------------------------------------------------------------------------
+            rotateMap(amount){
+                let map = this.$refs.map.$mapObject;
+                console.log('>>',this.$refs.map);
+                let heading = map.heading?map.heading + amount : amount;
+
+
+                map.setHeading(heading);
+                map.heading = heading;
+                map.panTo(this.center);
+
+            }
+            //-----------------------------------------------------------------------------
+
+        },
+    };
+</script>
+
+<style scoped>
+    .switches-section{
+        margin: 0;
+    }
+    .info-text{
+        color: #296fa8
+    }
+    label{
+        margin-left: 5px;
+    }
+</style>
---
+++ GoogleMap/README.md
@@ -0,0 +1,99 @@
+# GoogleMapMarker
+> Reusable Component
+
+
+### Introduction
+
+![Google Map Marker](./images/google_map_marker.png)
+
+GoogleMapMarker allows for quick integration of Google Maps and Google Map Markers to your project, while providing controls for other features such as
+   
+  * ##### Setting new locations as center: 
+    Navigate the click on the map to set latitude and longitude as center of the map (pan is added by default)
+    ![Set Map Center](./images/map_center.gif)
+
+  * ##### Adding Markers 
+    When add marker toggle is on, you can add markers by clicking on any location
+    ![Add Map Marker](./images/add_marker_demo.gif)
+
+  * ##### Removing Markers
+    Toggle Remove Marker and click on any marker to remove it
+    ![Remove Map Marker](./images/remove_marker_demo.gif)
+
+ 
+  * ##### Setting Custom Marker Icon
+    You can customise the icon image easily
+  
+  * #### Naming/Renaming Markers
+    Name/Rename Markers easily
+    ![Rename Map Marker](./images/rename_marker_demo.gif)
+
+    
+  * #### Adding Rotation Controls
+    Support for adding rotation buttons and for updation of icons
+    ![Rotate demo](./images/rotate_demo.gif)
+
+    
+  * #### Zoom Control
+    Native support for zoom
+
+---
+
+### Dependencies
+- [vue2-google-maps](https://github.com/xkjyeah/vue-google-maps)
+
+---
+
+
+### Implementation
+
+```vue
+    <google-map 
+        :latitude="new_item.latitude"
+        :longitude="new_item.longitude"
+        :image_url="root.image_url"
+        marker_icon="marker_icon.png"
+        rotate_left_icon="rotateleft.png"
+        rotate_right_icon="rotateright.png"
+    />
+```
+
+#### Provide API
+
+The parent component must provide the following properties using Provide/Inject API in Vue
+
+```vue
+    data(){
+        return {
+            markers: [],
+        }
+    },
+    provide(){
+        return {
+            markers: this.markers,
+            image_url: this.root.image_url
+        }
+    },
+```
+---
+
+### Properties
+
+| Name        | Description            | Type           | Values   | Default
+| -----------:|:----------------------:| --------------:|---------:|:------------
+| latitude    | latitude of the center | String,Number  | -        | -
+| longitude   | latitude of the center | String,Number  | -        | -
+| longitude   | latitude of the center | String,Number  | -        | -
+| zoom        | zoom level of the map  | String,Number  | -        | 16
+| custom_style| css style of the map (Must have width and height) | String  | -        | 'width:100%;  height: 400px;'
+| image_url | url for image location | String  | -        | -
+| marker_icon | name of marker image | String  | -        | -
+| rotate_left_icon | name of rotate left button image | String  | -        | -
+| rotate_right_icon | name of rotate right button image | String  | -        | -
+| map_type_id | type of map | String  | 'roadmap','satellite', 'hybrid', 'terrain'       | 'satellite'
+| show_controls | shows map controls | Boolean  | -    | true
+| is_rotatable | shows rotate controls | Boolean  | -    | true
+| heading | angle of rotation | String,Number  | -    | 0
+| tilt | tilt of map | String,Number  | -    | 0
+| is_edit_ready | sets add marker default to true and changes its switch label to edit | Boolean  | -    | false
+---
