3dfc64676dee2fe42a1e1b031511c4e092c66462
Kapil Singh <kapil-s001@webreinvent.com>
2023-11-06 13:20:47 +0530

    Updated: VvTags ui

--- vue-three/reusable/content-field/VvTags.vue
+++ vue-three/reusable/content-field/VvTags.vue
@@ -1,7 +1,21 @@
 <template>
 
     <div>
-        <Chips v-model="content_value" separator="," />
+        <Chips v-model="content_value"
+               separator=","
+               :placeholder="props.placeholder"
+               class="w-full is-small"
+               :pt="{
+                   container: {
+                       class: 'p-inputtext-sm'
+                   },
+                   inputtoken: ({ props }) => ({
+                       class: {
+                           'py-1': props.modelValue && props.modelValue.length !== 0
+                       }
+                   })
+               }"
+        />
     </div>
 
 </template>
@@ -41,6 +55,11 @@ const props = defineProps({
         default: null,
     },
 })
+
+const getPtData = (data) => {
+    console.log(data);
+    return true;
+}
 
 const content_value = computed({
     // getter
